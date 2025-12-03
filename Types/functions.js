9b30c1fd9d55dd025e566c37af441ca7e18f9a2d
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-03 13:41:21 +0530

    add examples of 	his behavior in arrow functions vs class methods

--- Types/functions.js
+++ Types/functions.js
@@ -66,8 +66,63 @@
 // IIFE
 // Immediately invoked function Expression
 //For an IIFE (Immediately Invoked Function Expression), we must wrap the function in parentheses.
-(function(){
-    console.log('running the anon functon ');
+// (function(){
+//     console.log('running the anon function ');
     
-    return 'you are cool'
-})();
+//     return 'you are cool'
+// })();
+
+//Methods!!
+// const wes={
+//  name:'wes bos',
+//  sayHi: function (){
+//     console.log("hey ");
+//     return 'hey wes';
+//  }
+// }
+
+//this in Arrow Function
+//Arrow functions do NOT have their own this
+//Instead, they inherit this from the parent scope (lexical this).
+//❌ In objects/classes, arrow functions cannot be used as methods (if you need this).
+// const user = {
+//    name: "Pratik",
+//    greet: () => {
+//      console.log(this.name); // ❌ undefined
+//    }
+//  };
+ 
+//  user.greet();
+ //👉 Because this in arrow function doesn’t refer to user.
+//It refers to the outer scope (global), not the object.
+
+//this in Regular Function (Inside Class/Object Method)
+// class User {
+//    constructor(name) {
+//      this.name = name;
+//    }
+ 
+//    greet() {
+//      console.log("Hello", this.name); // ✔ works
+//    }
+//  }
+ 
+//  const u = new User("Pratik");
+//  u.greet();
+ //Here this = instance of the class (u).
+
+ //Arrow Function INSIDE a Class
+ //Arrow functions are sometimes useful because they fix this to the instance.
+ class User {
+   constructor(name) {
+     this.name = name;
+   }
+ 
+   greet = () => {
+     console.log("Hi", this.name);
+   }
+ }
+ 
+ const u = new User("Pratik");
+ u.greet(); // Hi Pratik
+ 
