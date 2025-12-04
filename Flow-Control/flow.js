b2721ed0265b4d49d1c95b4b95d90c349a3c62bb
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-04 19:24:54 +0530

    add Flow control

---
+++ Flow-Control/flow.js
@@ -0,0 +1,46 @@
+// const age=25;
+// if(age > 20){
+//     console.log("in to your 25");
+
+// }else if (age>66){
+//     console.log("in to your 66");
+// }else if (age>50){
+//     console.log("in to your 25");
+// }
+
+
+// function slugify(sentence,lowercase){
+//     if(lowercase){
+//         return sentence.replace(/\s/g,'-').toLowerCase();
+//         /* What is \s ?
+//         \s matches any whitespace
+//         → spaces, tabs, line breaks, etc.
+//         What is g?
+//         g means global, so it replaces all matches, not just the first one.
+//         What does it do?
+//         It replaces all whitespace with dashes (-).
+//          */
+//     }
+//     else{
+//         return sentence.replace(/\s/g,'-');
+//     }
+// }
+
+// const name = "Pratik";  
+
+// //  or||  and &&
+
+// if (name ==="kadam" || name =="Pratik"){
+//     console.log("coool name");
+    
+// }
+
+//Truthy and Falsy
+
+const dog= "snickers";
+if(dog){
+    console.log("you have a dog ")
+}else {
+    console.log("you dont have a dog ");
+    
+}
